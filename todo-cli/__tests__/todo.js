eb3cc88d0ea6f7ed7d2c1f6561857d53d699ccac
shubhangiganti <gshubhangi2004@gmail.com>
2024-10-03 22:10:20 +0530

    commit for tests in L4

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,7 +1,6 @@
-/* eslint-disable no-undef */
 const todoList = require("../todo");
 const { all, markAsComplete, add } = todoList();
-denpmscribe("shubha test suite ", () => {
+describe("shubha test suite ", () => {
   test("should add new todo", () => {
     expect(all.length).toBe(0);
     add({
@@ -11,9 +10,37 @@ denpmscribe("shubha test suite ", () => {
     });
     expect(all.length).toBe(1);
   });
+
   test("should mark a todo as complete", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+
+  test("checks retrieval of overdue items", () => {
+    add({
+      title: "overdue task",
+      completed: false,
+      dueDate: yesterday,
+    });
+    expect(all[1].dueDate).toBe(yesterday);
+  });
+
+  test("checks retrieval of due Today items", () => {
+    add({
+      title: "duenow task",
+      completed: false,
+      dueDate: today,
+    });
+    expect(all[2].dueDate).toBe(today);
+  });
+
+  test("checks retrieval of due later items", () => {
+    add({
+      title: "laterdue task",
+      completed: false,
+      dueDate: tomorrow,
+    });
+    expect(all[3].dueDate).toBe(tomorrow);
   });
 });
