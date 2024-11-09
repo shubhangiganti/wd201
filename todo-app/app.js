958a855bf19e39e491045db23eca8e97997749e5
shubhangiganti <gshubhangi2004@gmail.com>
2024-11-09 12:14:34 +0530

    Testing pre-commit hook

--- todo-app/app.js
+++ todo-app/app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 app.get("/todos", (request, response) => {
-  console.log("Todo list hehee");
+  console.log("Todo list ");
 });
 
 app.post("/todos", async (request, response) => {
